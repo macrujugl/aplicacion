8f201df9c6e473e815c8b5a7365cb3d420174865
Mario Arienza <cowbotic@gmail.com>
2016-12-27 17:23:22 +0100

    cambios varios

--- app.sh
+++ app.sh
@@ -7,19 +7,33 @@ echo "---CUSTOM empieza---"
 export DEBIAN_FRONTEND=noninteractive
 apt-get -y update
 apt-get install -y lsscsi
-apt-get install -y 
 apt-get install -y python-pytest python-pip python-dev nginx
 
-echo "---CUSTOM pip y demas---"
+echo "---CUSTOM pip y virtualenv---"
+pip install virtualenv
+export APP_DIR=/home/mario/aplicacion
+mkdir -p $APP_DIR
+cd $APP_DIR
+virtualenv entorno
+chown -R mario:mario $APP_DIR
+source entorno/bin/activate
+
 pip install uwsgi flask
-PROJECT_NAME=flaskapp
-mkdir /opt/$PROJECT_NAME
-cd /opt/flaskr
-git clone  https://github.com/pallets/flask.git
-cd flask/examples/flaskr
-pip install --editable .
-export FLASK_APP=flaskr
-flask initdb
-flask run -h 0.0.0.0 -p 5001 &
+
+echo "---Clonando app---"
+git clone https://github.com/macrujugl/aplicacion
+mv ./aplicacion/* .
+chown mario:www-data *
+
+cp aplicacion.service /etc/systemd/system
+systemctl start aplicacion
+systemctl enable aplicacion
+
+cp aplicacion_web /etc/nginx/sites-available/
+ln -s /etc/nginx/sites-available/aplicacion_web /etc/nginx/sites-enabled
+rm /etc/nginx/sites-enabled/default
+
+systemctl restart nginx
+
 echo "---CUSTOM termina---"
 exit 0
