a8d76563c17a063acba2433e233c0726836f90fd
Mario Arienza <cowbotic@gmail.com>
2016-12-27 17:26:21 +0100

    Update de la tarde

--- aplicacion.py
+++ aplicacion.py
@@ -5,5 +5,13 @@ app = Flask(__name__)
 def hello():
     return "<h1 style='color:blue'>Hola Amiguitos!</h1>"
 
+@app.route("/check")
+def check():
+    return "<h1 style='color:red'>Estamos en check!</h1>"
+
+@app.route("/hi")
+def hi():
+    return "<h1 style='color:green'>Estamos en Hi!</h1>"
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
