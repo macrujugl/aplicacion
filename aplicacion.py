3456d7fc0a4abf9fb098806805ce3300aa60d002
Mario Arienza <cowbotic@gmail.com>
2017-01-13 12:57:26 +0100

    kkita

--- aplicacion.py
+++ aplicacion.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, request
 from azure.storage.table import TableService, Entity
-import socket, sys, random, time, math, 
+import socket, sys, random, time, math
 
 app = Flask(__name__)
 
